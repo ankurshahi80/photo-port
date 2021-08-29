05c2de42895689c2a3b65039b5a6f6b033b53dcc
Ankur <ankurshahi@Ankurs-iMac.local>
2021-08-29 13:30:45 -0400

    bug/unable to render ithe Gallery images

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -8,7 +8,7 @@ const Gallery =({currentCategory}) => {
         <section>
             <h1 data-testid = "h1tag">{capitalizeFirstLetter(name)}</h1>
             <p>{description}</p>
-            <PhotoList />
+            <PhotoList category={currentCategory.name} />
         </section>
     );
 }
